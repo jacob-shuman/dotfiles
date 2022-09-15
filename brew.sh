58003b651d6130f453403623672feff6942cba28
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2022-09-15 11:44:32 -0400

    feat: add homebrew config

---
+++ brew.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+# Install Git and GitHub CLI
+brew install git
+brew install gh
+
+# Install commonly used apps.
+brew install brave
+brew install discord
+brew install slack
+brew install figma
+
+# Install window manager
+brew install rectangle
+
+# Remove outdated versions from the cellar.
+brew cleanup
